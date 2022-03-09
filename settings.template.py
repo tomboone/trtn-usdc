60ebccfd7f111a381a40e88b3d9753ac4cf778e8
Tom Boone <boone@wrlc.org>
2022-03-09 13:36:44 -0500

    settings template comments

--- settings.template.py
+++ settings.template.py
@@ -1,5 +1,5 @@
 # To deploy, copy this file to settings.py and replace the values below
 
 url = 'SOLANA_NODE_ENDPOINT'  # URL of SOL node to query
-trtnacct = 'TRTN_ACCOUNT'  # replace with
+trtnacct = 'TRTN_ACCOUNT'  # replace with TRTN SOL account ID
 usdcacct = 'USDC_ACCOUNT'  # replace with USDC SOL account ID
